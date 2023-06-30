778e0e42ff6e22da018fcfa17f7e444676d18f20
Vinícius Sabino <viniaraujobh2000@gmail.com>
2023-06-30 16:42:30 -0300

    Criando classes para buscar os dados do campeonato e retornar o número de gols.

---
+++ Objetos/Championship.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questao2.Objetos
+{
+    public class Championship
+    {
+        [JsonProperty("competition")]
+        public string Competition { get; set; }
+
+        [JsonProperty("year")]
+        public int Year { get; set; }
+
+        [JsonProperty("round")]
+        public string Round { get; set; }
+
+        [JsonProperty("team1")]
+        public string Team1 { get; set; }
+
+        [JsonProperty("team2")]
+        public string Team2 { get; set; }
+
+        [JsonProperty("team1goals")]
+        public int Team1Goals { get; set; }
+
+        [JsonProperty("team2goals")]
+        public int Team2Goals { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using Questao2.Objetos;
+using System;
+using System.Net;
+
+public class Program
+{
+    private const string URL_DADOS_BASE = @"https://jsonmock.hackerrank.com/api/football_matches";
+
+    public static void Main()
+    {
+        string teamName = "Paris Saint-Germain";
+        int year = 2013;
+        int totalGoals = getTotalScoredGoals(teamName, year);
+
+        Console.WriteLine("Team "+ teamName +" scored "+ totalGoals.ToString() + " goals in "+ year);
+
+        teamName = "Chelsea";
+        year = 2014;
+        totalGoals = getTotalScoredGoals(teamName, year);
+
+        Console.WriteLine("Team " + teamName + " scored " + totalGoals.ToString() + " goals in " + year);
+
+        // Output expected:
+        // Team Paris Saint - Germain scored 109 goals in 2013
+        // Team Chelsea scored 92 goals in 2014
+    }
+
+    public static int getTotalScoredGoals(string team, int year)
+    {
+        Result result = GetCompetitionData(team, year);
+
+        int total = result.Data.Sum(q => q.Team1Goals);
+
+        return total;
+    }
+
+    public static Result GetCompetitionData(string team, int year)
+    {
+        Result result = new Result();
+
+        var request = (HttpWebRequest)WebRequest.Create(string.Concat(URL_DADOS_BASE, $"?year={year}&team1={team}"));
+        request.Method = "GET";
+
+        using (var response = request.GetResponse())
+        {
+            var streamDados = response.GetResponseStream();
+            StreamReader reader = new StreamReader(streamDados);
+            object objResponse = reader.ReadToEnd();
+            result = JsonConvert.DeserializeObject<Result>(objResponse.ToString());
+            streamDados.Close();
+            response.Close();
+        }
+
+        return result;
+    }
+}
